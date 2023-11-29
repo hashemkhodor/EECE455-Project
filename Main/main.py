836cc0982d11cd5658cda587d714a4b8d998d95f
hashemkhodor <87622346+hashemkhodor@users.noreply.github.com>
2023-11-29 19:53:04 +0200

    Changes

--- Main/main.py
+++ Main/main.py
@@ -7,3 +7,28 @@ app.static_folder = "static"
 @app.route("/", ["GET"])
 def _main_():
     return render_template()
+
+
+@app.route("/AES", ["GET", "POST"])
+def _main_():
+    return render_template()
+
+
+@app.route("/DES", ["GET", "POST"])
+def _main_():
+    return render_template()
+
+
+@app.route("/classical", ["GET", "POST"])
+def _main_():
+    return render_template()
+
+
+@app.route("/polynomial-arithmetic", ["GET", "POST"])
+def _main_():
+    return render_template()
+
+
+@app.route("/about", ["GET", "POST"])
+def _main_():
+    return render_template()
